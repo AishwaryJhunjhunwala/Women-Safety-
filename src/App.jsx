1bcd9735736cf769f909adaca66592fecc6a5cc7
aishwary jhunjhunwala <aishwaryjjw@gmail.com>
2025-09-06 15:23:00 +0530

    Initial Code

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import Navbar from "./components/Navbar/Navbar";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+    </div>
+  );
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function Navbar() {
+    return ( 
+        <div className="w-full bg-blue-500 p-4 text-white">
+            <h1>Navbar</h1>
+
+        </div>
+    );
+        
+}
+
+export default Navbar;
