92a9c1fc4baad2ad3bc99a97bb6f0dea15676e28
Anwesha.28 <sarkar.anwesha28@gmail.com>
2025-09-06 21:38:56 +0530

    UI_changes

--- app/page.jsx
+++ app/page.jsx
@@ -228,7 +228,7 @@ export default function HomePage() {
                 className="glass-card border-white/20 text-white hover:bg-white/10 h-14"
               >
                 <Phone className="w-5 h-5 mr-2" />
-                Call Contact
+                Call Emergency Contact
               </Button>
             </div>
           </div>
